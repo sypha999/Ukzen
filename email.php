1df2e5f55b4d6676bf5a3c6114e4dc426482a7f5
Sypha <datonarinze@gmail.com>
2022-08-09 23:31:32 +0100

    j

--- email.php
+++ email.php
@@ -1,12 +1,1 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "datonarinze@gmail.com";
-    $from = $_POST['email'];
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $message = $name ."with email " . $from . " sent the following message:" . "\n\n" . $_POST['message'];
-    $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    }
-?>
+
