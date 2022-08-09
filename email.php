2c4262919ccd6127caef718c6e2cf0878a6a7411
Sypha <datonarinze@gmail.com>
2022-08-09 22:37:12 +0100

    changes

--- email.php
+++ email.php
@@ -1,11 +1,10 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "datonarinze@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
+    $to = "datonarinze@gmail.com";
+    $from = $_POST['email'];
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $message = $name ."with email " . $from . " sent the following message:" . "\n\n" . $_POST['message'];
-
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
